314d754f7e85d609bbe7bd4bd95f1f334920f907
Riccardo Cavallari <riccardocavallari@gmail.com>
2018-01-30 20:52:30 +0100

    first commit

---
+++ Src/syscalls.c
@@ -0,0 +1,20 @@
+#include  <errno.h>
+#include  <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
+#include "stm32l0xx_hal.h"
+
+extern UART_HandleTypeDef huart2;
+int _write(int file, char *data, int len)
+{
+   if ((file != STDOUT_FILENO) && (file != STDERR_FILENO))
+   {
+      errno = EBADF;
+      return -1;
+   }
+
+   // arbitrary timeout 1000
+   HAL_StatusTypeDef status =
+      HAL_UART_Transmit(&huart2, (uint8_t*)data, len, 1000);
+
+   // return # of bytes written - as best we can tell
+   return (status == HAL_OK ? len : 0);
+}
